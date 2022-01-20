956494f22d254dc3356bc8c6b8f0dc622b783cfb
Antichess <72053073+Antichess@users.noreply.github.com>
2022-01-19 20:30:24 -0500

    Delete jfif to jpg.py

--- jfif to jpg.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-from PIL import Image
-import glob
-
-for file in glob.glob("*.jfif"):
-     im = Image.open(file)
-     rgb_im = im.convert('RGB')
-     rgb_im.save(file.replace("jfif", "jpeg"), quality=95)
